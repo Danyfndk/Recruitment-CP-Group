15ad9c45e911807ddcd75f231281308c0229c389
Danyfndk <danyfendika05@gmail.com>
2026-07-03 15:45:43 +0700

    Update database.py

--- sistem/database.py
+++ sistem/database.py
@@ -3,9 +3,12 @@ from supabase import create_client, Client
 
 @st.cache_resource
 def init_connection() -> Client:
-    # Kunci dimasukkan secara langsung (Hardcoded)
+    # URL mutlak tanpa tambahan karakter di belakangnya
     url = "https://lwiuemxgjttpirylkyem.supabase.co"
-    key = "sb_publishable_inPt1UjjFbAeOv49YkU07Q_AaCnxMrX"
+    
+    # Kunci Secret disisipkan langsung agar sistem bisa langsung diakses
+    key = "sb_publishable_inPtlUjjFbAeOv49YkU07Q_AaCnxMrX"
+    
     return create_client(url, key)
 
 supabase = init_connection()
