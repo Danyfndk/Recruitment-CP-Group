b2c98f19ad11f7e03962dc11410bcabb41db9461
Danyfndk <danyfendika05@gmail.com>
2026-07-03 14:56:01 +0700

    Update database.py

--- sistem/database.py
+++ sistem/database.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from supabase import create_client, Client
 
-# Fungsi ini bertugas memanggil kunci rahasia secara aman 
-# saat website dinyalakan nanti
 @st.cache_resource
 def init_connection() -> Client:
-    url = st.secrets[https://lwiuemxgjttpirylkyem.supabase.co/rest/v1/]
-    key = st.secrets[sb_publishable_inPtlUjjFbAeOv49YkU07Q_AaCnxMrX]
+    # Kunci dimasukkan secara langsung (Hardcoded)
+    url = "https://lwiuemxgjttpirylkyem.supabase.co"
+    key = "sb_publishable_inPt1UjjFbAeOv49YkU07Q_AaCnxMrX"
     return create_client(url, key)
 
-# Menyalakan mesin database
 supabase = init_connection()
